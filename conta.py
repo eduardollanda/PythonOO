fcf2e623c1976d22e3a05f2e4f79aed3585508db
eduardollanda <eduardohollanda14@gmail.com>
2020-04-18 02:36:17 -0300

    atualizando estudo

--- conta.py
+++ conta.py
@@ -1,4 +1,18 @@
 class Conta:
-    
-    def __init__(self):
-         print("contruindo objeto ...{}".format(self))
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("contruindo objeto ...{}".format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+
+    def extrato(self):
+        print("saldo de {} do titular {}".format(self.saldo, self.titular))
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saque(self, valor):
+        if valor < self.saldo:
+            self.saldo -= valor
