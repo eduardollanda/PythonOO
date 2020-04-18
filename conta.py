1b944007dad6c750aa55935a8cd2e2ae947a4306
eduardollanda <eduardohollanda14@gmail.com>
2020-04-18 04:02:12 -0300

    introduzindo getter e setter

--- conta.py
+++ conta.py
@@ -20,3 +20,21 @@ class Conta:
     def transfere(self, valor, destino):
         self.saque(valor)
         destino.deposita(valor)
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    def get_numero(self):
+        return self.__numero
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
